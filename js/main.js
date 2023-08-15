2ed77a43f519e6e27282cad2c9e3b9c00d1f3e8b
Juan Perez <juan-ks@hotmail.es>
2023-08-15 11:19:15 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,505 @@
+const productos = [
+    {
+        id: "cable1",
+        titulo: "cable1",
+        imagen: "../img/productos/1.jpg",
+        link: "../../index.html",
+        title: "Cable Acsr",
+        descripcion: "Cable Acsr Con alma de acero",
+        categoria: {
+            nombre: "Cables",
+            id: "cablesyalambres"
+        }
+    },
+
+    {
+        id: "cable2",
+        titulo: "cable2",
+        imagen: "../img/productos/2.jpg",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Aislado",
+        categoria: {
+            nombre: "Cables",
+            id: "cablesyalambres"
+        }
+    },
+
+    {
+        id: "cable3",
+        titulo: "cable3",
+        imagen: "../img/productos/3.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Cables",
+            id: "cablesyalambres"
+        }
+    },
+
+    {
+        id: "cable4",
+        titulo: "cable4",
+        imagen: "../img/productos/4.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Cables",
+            id: "cablesyalambres"
+        }
+    },
+
+    {
+        id: "cable5",
+        titulo: "cable5",
+        imagen: "../img/productos/5.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Cables",
+            id: "cablesyalambres"
+        }
+    },
+
+    {
+        id: "cable6",
+        titulo: "cable6",
+        imagen: "../img/cable6.jpg",
+        categoria: {
+            nombre: "Cables",
+            id: "cablesyalambres"
+        }
+    },
+
+    {
+        id: "tableros",
+        titulo: "herramienta1",
+        imagen: "../img/productos/6.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Tableros",
+            id: "tableros"
+        }
+    },
+
+    {
+        id: "herramienta2",
+        titulo: "herramienta2",
+        imagen: "../img/productos/7.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Tableros",
+            id: "tableros"
+        }
+    },
+
+    {
+        id: "herramienta3",
+        titulo: "herramienta3",
+        imagen: "../img/productos/8.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Tableros",
+            id: "tableros"
+        }
+    },
+
+    {
+        id: "herramientas4",
+        titulo: "herramientas4",
+        imagen: "../img/productos/9.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Tableros",
+            id: "tableros"
+        }
+    },
+
+    {
+        id: "herramientas5",
+        titulo: "herramientas5",
+        imagen: "../img/productos/10.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Tableros",
+            id: "tableros"
+        }
+    },
+
+    {
+        id: "herramientas6",
+        titulo: "herramientas6",
+        imagen: "../img/productos/11.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Tableros",
+            id: "tableros"
+        }
+    },
+
+    {
+        id: "herramientas6",
+        titulo: "herramientas6",
+        imagen: "../img/productos/12.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Tableros",
+            id: "tableros"
+        }
+    },
+
+    {
+        id: "herramientas6",
+        titulo: "herramientas6",
+        imagen: "../img/productos/13.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Tableros",
+            id: "tableros"
+        }
+    },
+
+    {
+        id: "herramientas6",
+        titulo: "herramientas6",
+        imagen: "../img/productos/14.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Tableros",
+            id: "tableros"
+        }
+    },
+
+    {
+        id: "interruptor",
+        titulo: "herramientas6",
+        imagen: "../img/productos/15.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Interruptores Y Tomas Electricos",
+            id: "interruptores"
+        }
+    },
+
+    {
+        id: "interruptor",
+        titulo: "herramientas6",
+        imagen: "../img/productos/16.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Interruptores Y Tomas Electricos",
+            id: "interruptores"
+        }
+    },
+
+    {
+        id: "interruptor",
+        titulo: "herramientas6",
+        imagen: "../img/productos/17.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Interruptores Y Tomas Electricos",
+            id: "interruptores"
+        }
+    },
+
+    {
+        id: "interruptor",
+        titulo: "herramientas6",
+        imagen: "../img/productos/18.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Interruptores Y Tomas Electricos",
+            id: "interruptores"
+        }
+    },
+
+    {
+        id: "interruptor",
+        titulo: "herramientas6",
+        imagen: "../img/productos/19.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Interruptores Y Tomas Electricos",
+            id: "interruptores"
+        }
+    },
+
+    {
+        id: "interruptor",
+        titulo: "herramientas6",
+        imagen: "../img/productos/20.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Interruptores Y Tomas Electricos",
+            id: "interruptores"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/22.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/23.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+    
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/24.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/25.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/26.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/27.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/28.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/29.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/30.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/31.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/32.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/33.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    },
+
+    {
+        id: "gabinetes",
+        titulo: "herramientas6",
+        imagen: "../img/productos/34.png",
+        link: "../../index.html",
+        title: "Cable Aluminio",
+        descripcion: "Triplex Y Cuadruplex",
+        categoria: {
+            nombre: "Gabinetes y Equipos de medida",
+            id: "gabinetes"
+        }
+    }
+]
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+        <div class="card" style="width: 14rem;">
+            <img src="${producto.imagen}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${producto.title}</h5>
+                <p class="card-text">${producto.descripcion}</p>
+            <a href="${producto.link}" class="btn btn-warning">Cotizar ahora</a>
+        </div>
+        `
+        contenedorProductos.append(div)
+    })
+
+}
+
+cargarProductos(productos);
+
+
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+    })
+});
+
+
+/*
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="${producto.imagen}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${producto.title}</h5>
+        <p class="card-text">${producto.descripcion}</p>
+      </div>
+    </div>
+  </div>
+</div>
+*/
